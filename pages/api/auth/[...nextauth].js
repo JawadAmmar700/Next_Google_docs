d513908e7df89fe626cb323f759ba3d87cee45ef
jawadGitNext <jawadGitNext2021@gmail.com>
2021-09-26 19:55:50 +0300

    fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,12 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-      clientId:
-        '631721808685-bbjpo4lkp079m7t3ubqj5jh6jbt1i46o.apps.googleusercontent.com',
-      clientSecret: 'xYQ9yPbzfteq3yrh84L6WS5P',
+      clientId: process.env.GOOGLE_API_ID,
+      clientSecret: process.env.GOOGLE_API_SECRET,
     }),
     // ...add more providers here
   ],
