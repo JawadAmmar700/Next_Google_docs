8c20793827c376113a7c53edfd99825adcee8c4d
jawadGitNext <jawadGitNext2021@gmail.com>
2021-09-26 21:26:00 +0300

    new firebase

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIqxC14nytj52dEENY9jkEho_Q9heteWE",
-  authDomain: "docs-8a1a5.firebaseapp.com",
-  projectId: "docs-8a1a5",
-  storageBucket: "docs-8a1a5.appspot.com",
-  messagingSenderId: "60186955549",
-  appId: "1:60186955549:web:bd50de323b58b5df50dbf4",
-  measurementId: "G-CNR7L3WL35",
+  apiKey: "AIzaSyC8bjrWpRPFJWDxlGgNcgShiURDJnhXALo",
+  authDomain: "docs-16c42.firebaseapp.com",
+  projectId: "docs-16c42",
+  storageBucket: "docs-16c42.appspot.com",
+  messagingSenderId: "905555126308",
+  appId: "1:905555126308:web:0a8c9116c881c3372290b1",
+  measurementId: "G-G92WV6JZTJ",
 }
 
 const app = !firebase.apps.length
